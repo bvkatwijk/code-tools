200c48cb1ba27d9ed98e15589f5765b11e98d4fc
bvkatwijk <BorisK@topdesk.com>
2017-10-06 16:20:10 +0200

    Input code in input component

--- src/app/components/code-input/code-input.component.ts
+++ src/app/components/code-input/code-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-code-input',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodeInputComponent implements OnInit {
 
+  @Input() code;
+  @Output() codeChange = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  setCode(code: string) {
+    this.code = code;
+    this.codeChange.emit(code);
+  }
+
+  
 
 }
