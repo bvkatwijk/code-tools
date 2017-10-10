30bfdb410161065f8c9158fd50a033850522c5c3
bvkatwijk <BorisK@topdesk.com>
2017-10-10 16:21:40 +0200

    Add test for specifying constructor fields

--- src/app/components/builder/fluid-builder/converter/trait/build/fb-build-method.spec.ts
+++ src/app/components/builder/fluid-builder/converter/trait/build/fb-build-method.spec.ts
@@ -2,22 +2,40 @@ import { Build } from './fb-build';
 
 describe("Build Method", () => {
 
-    it('should be created for target named target', () => {
+    it('should be created for target named target and empty field', () => {
         let expectedTarget =
-        "@Override"
-		+ "\npublic Target build() {"
-		+ "\n\treturn new Target();"
-		+ "\n}";
-        expect(new Build('target').method()).toEqual(expectedTarget);
+            "@Override"
+            + "\npublic Target build() {"
+            + "\n\treturn new Target();"
+            + "\n}";
+        expect(new Build('target').method('')).toEqual(expectedTarget);
     });
 
-    it('should be created for target named target', () => {
+    it('should be created for target named target and empty field', () => {
         let expectedName =
-        "@Override"
-		+ "\npublic Name build() {"
-		+ "\n\treturn new Name();"
-		+ "\n}";
-        expect(new Build('name').method()).toEqual(expectedName);
+            "@Override"
+            + "\npublic Name build() {"
+            + "\n\treturn new Name();"
+            + "\n}";
+        expect(new Build('name').method('')).toEqual(expectedName);
+    });
+
+    it('should be created for target named target and some field', () => {
+        let expectedTarget =
+            "@Override"
+            + "\npublic Target build() {"
+            + "\n\treturn new Target(some);"
+            + "\n}";
+        expect(new Build('target').method('some')).toEqual(expectedTarget);
+    });
+
+    it('should be created for target named target and some field', () => {
+        let expectedName =
+            "@Override"
+            + "\npublic Name build() {"
+            + "\n\treturn new Name(some);"
+            + "\n}";
+        expect(new Build('name').method('some')).toEqual(expectedName);
     });
 
 });
