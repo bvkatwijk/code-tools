64fb88fdf300e31845d3480e8386c4712c99c256
Unknown <bvkatwijk@gmail.com>
2017-10-12 11:11:48 +0200

    Test capitalizer

---
+++ src/app/classes/capitalize/capitalizer.spec.ts
@@ -0,0 +1,21 @@
+import { capitalize } from './capitalizer';
+
+describe("Capitalizer", () => {
+
+    it("converts string to String", () => {
+        expect(capitalize('string')).toEqual('String');
+    });
+
+    it("does not affect String", () => {
+        expect(capitalize('String')).toEqual('String');
+    });
+
+    it("converts a string to A string", () => {
+        expect(capitalize('a string')).toEqual('A string');
+    });
+
+    it("does not affect A string", () => {
+        expect(capitalize('A string')).toEqual('A string');
+    });
+
+});
