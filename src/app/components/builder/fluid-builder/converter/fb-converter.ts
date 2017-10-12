967c0b16d8a782f97204d1518c7ecf6b79ead30b
Unknown <bvkatwijk@gmail.com>
2017-10-12 16:58:01 +0200

    Isolate parts of generation into methods

--- src/app/components/builder/fluid-builder/converter/fb-converter.ts
+++ src/app/components/builder/fluid-builder/converter/fb-converter.ts
@@ -1,3 +1,4 @@
+import { Field } from '../../../../classes/parse/field/field';
 import { With } from './trait/with/fb-with';
 import { JavaParser } from 'app/classes/parse/parse';
 import { Build } from 'app/components/builder/fluid-builder/converter/trait/build/fb-build';
@@ -12,6 +13,7 @@ export class FluidBuilderConverter {
         const result = new JavaParser(value);
         const fields = result.getFields();
         const target = result.getName();
+
         const methods: string[] = [];
         const traits: string[] = [];
         for (let i = 0; i < fields.length; i++) {
@@ -28,36 +30,71 @@ export class FluidBuilderConverter {
 
         const targetBuild = new Build(target, this.indenter);
 
-        return `package org.bvkatwijk.fbg.sample;
-
-import lombok.Value;
-
-@Value
-public class SingleFieldSample {
-
-    private final String firstField;
-
-    public static WithFirstField builder() {
-        return new SingleFieldSampleBuilder();
+        return [
+            this.packageDeclaration(),
+            this.importStatements(),
+            this.sourceClassDeclaration(),
+            this.sourceClassBody(fields, methods, targetBuild, traits),
+            '}',
+        ].join('\n\n') + '\n';
     }
 
-    /** 2017-08-06 Generated Fluid Builder github.com/bvkatwijk/fluid-builder-generator */
-    public static class SingleFieldSampleBuilder implements WithFirstField, BuildSingleFieldSample {
-
-` + this.indenter.indent(this.indenter.indent(fields[0].mutableDeclaration() + `
-
-` + methods[0] + `
-
-` + targetBuild.method('firstField'))) + `
-
+    private builderClassDeclaration(): string {
+        return '/** 2017-08-06 Generated Fluid Builder github.com/bvkatwijk/fluid-builder-generator */'
+            + '\npublic static class SingleFieldSampleBuilder implements WithFirstField, BuildSingleFieldSample {';
     }
 
-` + this.indenter.indent(traits[0] + `
+    private builderClassBody(fields: Field[], methods: string[], targetBuild: any): string {
+        return this
+            .indenter
+            .indent([
+            this.mutableFieldDeclarations(fields),
+                methods.join('\n\n'),
+                targetBuild.method('firstField')
+            ].join('\n\n'));
+    }
 
-` + targetBuild.trait()) + `
+    private builderMethodDeclaration(): string {
+        return 'public static WithFirstField builder() {'
+            + '\n' + this.indenter.indent('return new SingleFieldSampleBuilder();')
+            + '\n}';
+    }
 
-}
-`;
+    private packageDeclaration(): string {
+        return 'package org.bvkatwijk.fbg.sample;';
+    }
+
+    private importStatements(): string {
+        return 'import lombok.Value;';
+    }
+
+    private sourceClassDeclaration(): string {
+        return '@Value'
+            + '\npublic class SingleFieldSample {';
+    }
+
+    private sourceClassBody(fields: Field[], methods: string[], targetBuild: any, traits: any[]): string {
+        return this.indenter.indent([
+            this.immutableFieldDeclarations(fields),
+            this.builderMethodDeclaration(),
+            this.builderClassDeclaration(),
+            this.builderClassBody(fields, methods, targetBuild),
+            '}',
+            traits.join('\n\n'),
+            targetBuild.trait()
+        ].join('\n\n'));
+    }
+
+    private immutableFieldDeclarations(fields: Field[]): string {
+        return fields
+            .map(it => it.immutableDeclaration())
+            .join('\n');
+    }
+
+    private mutableFieldDeclarations(fields: Field[]): string {
+        return fields
+            .map(it => it.mutableDeclaration())
+            .join('\n');
     }
 
 }
