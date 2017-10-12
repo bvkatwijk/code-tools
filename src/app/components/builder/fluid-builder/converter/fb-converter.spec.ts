d146f6037f704702c794d02326802f05e793bbf0
Unknown <bvkatwijk@gmail.com>
2017-10-12 21:39:46 +0200

    Add second full example test

--- src/app/components/builder/fluid-builder/converter/fb-converter.spec.ts
+++ src/app/components/builder/fluid-builder/converter/fb-converter.spec.ts
@@ -7,7 +7,7 @@ describe('Fluid Builder Converter', () => {
 
     describe('first example', () => {
 
-        const singleFieldClassSource = `package org.bvkatwijk.fbg.sample;
+        const source = `package org.bvkatwijk.fbg.sample;
 
 import lombok.Value;
 
@@ -19,7 +19,7 @@ public class SingleFieldSample {
 }
 `;
 
-        const singleFieldClassTarget = `package org.bvkatwijk.fbg.sample;
+        const target = `package org.bvkatwijk.fbg.sample;
 
 import lombok.Value;
 
@@ -63,8 +63,72 @@ public class SingleFieldSample {
 
         it('should be converted to valid fluid builder implementation', () => {
             new StringHelper().assertEquals(
-                new FluidBuilderConverter().convert(singleFieldClassSource),
-                singleFieldClassTarget);
+                new FluidBuilderConverter().convert(source),
+                target);
+        });
+
+    });
+
+    describe('second example', () => {
+
+        const source = `package com.some.other.pack;
+
+import lombok.Value;
+
+@Value
+public class SingleFieldSample {
+    
+    private final String firstField;
+
+}
+`;
+
+        const target = `package com.some.other.pack;
+
+import lombok.Value;
+
+@Value
+public class SingleFieldSample {
+
+    private final String firstField;
+
+    public static WithFirstField builder() {
+        return new SingleFieldSampleBuilder();
+    }
+
+    /** 2017-08-06 Generated Fluid Builder github.com/bvkatwijk/fluid-builder-generator */
+    public static class SingleFieldSampleBuilder implements WithFirstField, BuildSingleFieldSample {
+
+        private String firstField;
+
+        @Override
+        public BuildSingleFieldSample firstField(String firstField) {
+            this.firstField = firstField;
+            return this;
+        }
+
+        @Override
+        public SingleFieldSample build() {
+            return new SingleFieldSample(firstField);
+        }
+
+    }
+
+    public static interface WithFirstField {
+        public BuildSingleFieldSample firstField(String firstField);
+    }
+
+    public static interface BuildSingleFieldSample {
+        public SingleFieldSample build();
+    }
+
+}
+`;
+
+        it('should be converted to valid fluid builder implementation', () => {
+            new StringHelper().assertEquals(
+                new FluidBuilderConverter().convert(source),
+                target);
         });
 
     });
