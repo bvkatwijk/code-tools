24ec6222b3391b0ace9bbd7db82c676fc118537a
bvkatwijk <BorisK@topdesk.com>
2017-10-13 13:01:00 +0200

    Add import tests

---
+++ src/app/components/builder/fluid-builder/converter/import/import.spec.ts
@@ -0,0 +1,101 @@
+import { Import } from "app/components/builder/fluid-builder/converter/import/import";
+
+describe("Import", () => {
+
+    const A = JSON.parse(`[
+        {
+            "node": "ImportDeclaration",
+            "name": {
+                "identifier": "A",
+                "node": "SimpleName"
+            },
+            "static": false,
+            "onDemand": false
+        }
+    ]`);
+
+    const aB = JSON.parse(`[
+            {
+                "node": "ImportDeclaration",
+                "name": {
+                    "node": "QualifiedName",
+                    "qualifier": {
+                        "identifier": "a",
+                        "node": "SimpleName"
+                    },
+                    "name": {
+                        "identifier": "B",
+                        "node": "SimpleName"
+                    }
+                },
+                "static": false,
+                "onDemand": false
+            }
+        ]`);
+
+    const abC = JSON.parse(`[
+                {
+                    "node": "ImportDeclaration",
+                    "name": {
+                        "node": "QualifiedName",
+                        "qualifier": {
+                            "node": "QualifiedName",
+                            "qualifier": {
+                                "identifier": "a",
+                                "node": "SimpleName"
+                            },
+                            "name": {
+                                "identifier": "b",
+                                "node": "SimpleName"
+                            }
+                        },
+                        "name": {
+                            "identifier": "C",
+                            "node": "SimpleName"
+                        }
+                    },
+                    "static": false,
+                    "onDemand": false
+                }
+            ]`);
+
+    it('should parse import A', () => {
+        expect(new Import(A).getStatement()).toEqual('import A;');
+    });
+
+    it('should parse import a.B', () => {
+        expect(new Import(aB).getStatement()).toEqual('import a.B;');
+    });
+
+    it('should parse import a.B', () => {
+        expect(new Import(abC).getStatement()).toEqual('import a.b.C;');
+    });
+
+    describe("static", () => {
+
+        const staticAb = JSON.parse(`[
+            {
+                "node": "ImportDeclaration",
+                "name": {
+                    "node": "QualifiedName",
+                    "qualifier": {
+                        "identifier": "A",
+                        "node": "SimpleName"
+                    },
+                    "name": {
+                        "identifier": "b",
+                        "node": "SimpleName"
+                    }
+                },
+                "static": true,
+                "onDemand": false
+            }
+        ]`);
+
+        it('should parse import static A.b;', () => {
+            expect(new Import(staticAb).getStatement()).toEqual('import static A.b;');
+        })
+
+    })
+
+});
