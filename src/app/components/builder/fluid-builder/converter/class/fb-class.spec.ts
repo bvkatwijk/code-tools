cd11bed0bdd234ff60577868d6bbab6457bacb9b
bvkatwijk <BorisK@topdesk.com>
2017-10-13 14:55:40 +0200

    Add elaborate testcase

--- src/app/components/builder/fluid-builder/converter/class/fb-class.spec.ts
+++ src/app/components/builder/fluid-builder/converter/class/fb-class.spec.ts
@@ -1,3 +1,4 @@
+import { capitalize } from '../../../../../classes/capitalize/capitalizer';
 import { With } from '../trait/with/fb-with';
 import { Indenter } from '../../../../../classes/indent/indenter';
 import { Build } from '../trait/build/fb-build';
@@ -6,9 +7,13 @@ import { PRIVATE, PUBLIC } from '../../../../../classes/parse/visibility/visibil
 import { StringHelper } from '../../../../../classes/string/string-helper.spec';
 import { FluidBuilderClass } from './fb-class';
 
-describe(" ", () => {
+describe('Fluid Builder Class', () => {
+
     let indenter = new Indenter('    ');
-    let builderClassTarget = `/** 2017-08-06 Generated Fluid Builder github.com/bvkatwijk/fluid-builder-generator */
+    let field = new Field(PRIVATE, false, 'String', 'firstField');
+
+    describe('single field', () => {
+        let target = `/** 2017-08-06 Generated Fluid Builder github.com/bvkatwijk/fluid-builder-generator */
 public static class SingleFieldSampleBuilder implements WithFirstField, BuildSingleFieldSample {
 
     private String firstField;
@@ -26,14 +31,63 @@ public static class SingleFieldSampleBuilder implements WithFirstField, BuildSin
 
 }`
 
-    it('should generate builder class correctly', () => {
-        const field = new Field(PRIVATE, false, 'String', 'firstField');
-        new StringHelper().assertEquals(
-            new FluidBuilderClass(indenter).declarationAndBody(
-                [field],
-                [new With(field.name, 'BuildSingleFieldSample', indenter).method()],
-                new Build('SingleFieldSample', indenter)),
-            builderClassTarget);
+        it('should generate builder class correctly', () => {
+            new StringHelper().assertEquals(
+                new FluidBuilderClass(indenter).declarationAndBody(
+                    [
+                        field
+                    ],
+                    [
+                        new With(field, 'BuildSingleFieldSample', indenter)
+                    ],
+                    new Build('SingleFieldSample', indenter)),
+                target);
+        });
+
+    });
+
+    describe('two private fields', () => {
+
+        let secondField = new Field(PRIVATE, false, 'Integer', 'aNumber');
+        let target = `/** 2017-08-06 Generated Fluid Builder github.com/bvkatwijk/fluid-builder-generator */
+public static class SingleFieldSampleBuilder implements WithFirstField, WithANumber, BuildSingleFieldSample {
+
+    private String firstField;
+    private Integer aNumber;
+
+    @Override
+    public WithANumber firstField(String firstField) {
+        this.firstField = firstField;
+        return this;
+    }
+
+    @Override
+    public BuildSingleFieldSample aNumber(Integer aNumber) {
+        this.aNumber = aNumber;
+        return this;
+    }
+
+    @Override
+    public SingleFieldSample build() {
+        return new SingleFieldSample(firstField, aNumber);
+    }
+
+}`
+
+        it('should generate builder class correctly', () => {
+            new StringHelper().assertEquals(
+                new FluidBuilderClass(indenter).declarationAndBody(
+                    [
+                        field,
+                        secondField],
+                    [
+                        new With(field, 'With' + capitalize(secondField.name), indenter),
+                        new With(secondField, 'BuildSingleFieldSample', indenter)
+                    ],
+                    new Build('SingleFieldSample', indenter)),
+                target);
+        });
+
     });
 
 });
