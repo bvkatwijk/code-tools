f219581e5ac0a644e339549078f8d5ed6af960ec
bvkatwijk <BorisK@topdesk.com>
2017-10-13 11:02:28 +0200

    Use functional implementation

--- src/app/components/builder/fluid-builder/converter/pack/fb-pack.ts
+++ src/app/components/builder/fluid-builder/converter/pack/fb-pack.ts
@@ -8,23 +8,36 @@ export class Package {
 
     }
 
+    /**
+     * return package declaration
+     */
     getDeclaration(): string {
         return 'package ' + this.getPackageName(this.packageJson.name) + ';';
     }
 
-    getPackageName(it: any): string {
-        let packageName = '';
-        if (it.identifier) {
-            packageName = it.identifier;
-        }
-        if (it.name) {
-            packageName = this.getPackageName(it.name) + packageName;
-        }
-        if (it.qualifier) {
-            packageName = this.getPackageName(it.qualifier) + '.' + packageName;
-        }
-        return packageName;
+    private getPackageName(it: any): string {
+        return [it]
+            .map(it => this.getFromIdentifier(it))
+            .map(result => this.prependFromName(result, it))
+            .map(result => this.prependFromQualifier(result, it))[0];
     }
 
+    private prependFromQualifier(packageName, it: any): string {
+        return it.qualifier
+            ? this.getPackageName(it.qualifier) + '.' + packageName
+            : packageName
+    }
+
+    private prependFromName(packageName: string, it: any): string {
+        return it.name
+            ? this.getPackageName(it.name) + packageName
+            : packageName;
+    }
+
+    private getFromIdentifier(it: any): string {
+        return it.identifier
+            ? it.identifier
+            : '';
+    }
 
 }
