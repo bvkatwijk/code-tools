b235c0b9a7062a2d46226a1c65d25974877d8450
Boris van Katwijk <bvkatwijk@gmail.com>
2018-10-02 11:06:59 +0200

    Validate generated builder method

--- src/app/components/builder/fluid-builder/converter/fb-converter.spec.ts
+++ src/app/components/builder/fluid-builder/converter/fb-converter.spec.ts
@@ -1,9 +1,34 @@
 import { StringHelper } from '../../../../classes/string/string-helper.spec';
 import { Whitespace } from '../../../../classes/whitespace/whitespace';
 import { FluidBuilderConverter } from './fb-converter';
+import { Field } from '../../../../classes/parse/field/field';
+import { Visibility, PUBLIC } from '../../../../classes/parse/visibility/visibility';
 
 
 describe('Fluid Builder Converter', () => {
+
+    describe('#withFirstField', () => {
+        describe('on "name"', () => {
+            const fieldName = 'name';
+            it('returns "WithName"', () => {
+                expect(new FluidBuilderConverter().withFirstField([new Field(
+                    PUBLIC,
+                    true,
+                    'String',
+                    fieldName)])).toBe('WithName');
+            });
+        });
+        describe('on "other"', () => {
+            const fieldName = 'other';
+            it('returns "WithOther"', () => {
+                expect(new FluidBuilderConverter().withFirstField([new Field(
+                    PUBLIC,
+                    true,
+                    'String',
+                    fieldName)])).toBe('WithOther');
+            });
+        });
+    });
 
     describe('first example', () => {
 
@@ -146,4 +171,76 @@ public class SingleFieldSample {
 
     });
 
+    describe('example without a package', () => {
+
+        const source = `import some.other.Import;
+
+@Value
+public class SingleFieldSample {
+
+    private final String firstField;
+    private final Integer aNumber;
+
+}
+`;
+
+        const target = `import some.other.Import;
+
+@Value
+public class SingleFieldSample {
+
+    private final String firstField;
+    private final Integer aNumber;
+
+    public static WithFirstField builder() {
+        return new SingleFieldSampleBuilder();
+    }
+
+    /** 2017-08-06 Generated Fluid Builder github.com/bvkatwijk/fluid-builder-generator */
+    public static class SingleFieldSampleBuilder implements WithFirstField, WithANumber, BuildSingleFieldSample {
+
+        private String firstField;
+        private Integer aNumber;
+
+        @Override
+        public WithANumber firstField(String firstField) {
+            this.firstField = firstField;
+            return this;
+        }
+
+        @Override
+        public BuildSingleFieldSample aNumber(Integer aNumber) {
+            this.aNumber = aNumber;
+            return this;
+        }
+
+        @Override
+        public SingleFieldSample build() {
+            return new SingleFieldSample(firstField, aNumber);
+        }
+
+    }
+
+    public static interface WithFirstField {
+        public WithANumber firstField(String firstField);
+    }
+
+    public static interface WithANumber {
+        public BuildSingleFieldSample aNumber(Integer aNumber);
+    }
+
+    public static interface BuildSingleFieldSample {
+        public SingleFieldSample build();
+    }
+
+}
+`;
+
+        it('should be converted to valid fluid builder implementation', () => {
+            new StringHelper().assertEquals(
+                new FluidBuilderConverter().convert(source),
+                target);
+        });
+
+    });
 });
